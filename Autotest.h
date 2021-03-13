af0fbd6dc263d9036eeb9d1dd51a66b5c4fabf50
Mihail Tsybakov <mihailtsybakov@yandex.ru>
2021-03-13 23:59:16 +0300

    Added test exception

--- Autotest.h
+++ Autotest.h
@@ -1,8 +1,29 @@
 #ifndef TEST
 #define TEST
+
 #include "CIntN.h"
 #include "CIntN_hori.h"
 #include "CIntN_vert.h"
+
+#include <sstream>
+
 bool autotest();
+
+class test_error : public exception
+{
+public:
+	int test_number, test_result;
+	test_error(int test_number, int test_result)
+	{
+		this->test_number = test_number;
+		this->test_result = test_result;
+	}
+	const char* what() const noexcept override
+	{
+		stringstream ss;
+		ss << "Failed test " << test_number << ": wrong result is " << test_result << endl;
+		return (ss.str()).c_str();
+	}
+};
 
 #endif // TEST
